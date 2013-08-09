ea23df8c8e7a0a48223a88331ae49686cefc3135
Sean Ballew <sballew7@gmail.com>
2013-08-08 21:49:36 -0700

    Fixing whitespace.

--- src/Abodeo/LaravelStripe/LaravelStripeServiceProvider.php
+++ src/Abodeo/LaravelStripe/LaravelStripeServiceProvider.php
@@ -31,11 +31,11 @@ class LaravelStripeServiceProvider extends ServiceProvider {
 		// Register blade compiler for the Stripe publishable key.
 		$blade = $this->app['view']->getEngineResolver()->resolve('blade')->getCompiler();
 		$blade->extend(function($value, $compiler) use($publishableKey)
-        {
-            $matcher = "/(?<!\w)(\s*)@stripeKey/";
+		{
+			$matcher = "/(?<!\w)(\s*)@stripeKey/";
             
-            return preg_replace($matcher, $publishableKey, $value);
-        });
+			return preg_replace($matcher, $publishableKey, $value);
+		});
 	}
 
 	/**
